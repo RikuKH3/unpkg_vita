3c83525e82fcfb410b6530fc56ae1ccee2409e60
RikuKH3 <riku.kh3@gmail.com>
2017-09-26 16:01:32 +0300

    Update unpkg_vita.dpr

--- unpkg_vita.dpr
+++ unpkg_vita.dpr
@@ -8,9 +8,14 @@ program unpkg_vita;
 {$R *.res}
 
 uses
-  Windows, System.SysUtils, System.Classes, IOUtils, DCPrijndael, DCPcrypt2;
+  Windows, System.SysUtils, System.Classes, System.NetEncoding, IOUtils, DCPrijndael, DCPcrypt2, Zlib in 'Zlib.pas';
 
 {$SETPEFLAGS IMAGE_FILE_RELOCS_STRIPPED}
+
+var
+  WorkbinExist: Byte;
+  WorkbinKey: array [0..15] of Byte;
+  Workbin: array [0..511] of Byte;
 
 function Swap64(Value: Int64): Int64; register; overload;
 asm
@@ -41,31 +46,77 @@ begin
   end;
 end;
 
-function ProcessSFO(MemoryStream1: TMemoryStream): UTF8String;
+function ProcessSFO(MemoryStream1: TMemoryStream): Word;
 var
-  NameStart, ValueStart, NumOfEntries, LongWord1: LongWord;
+  NameStart, ValueStart, NumOfEntries, LongWord1, InitPos: LongWord;
+  Int641: Int64;
   Word1: Word;
 begin
-  MemoryStream1.ReadBuffer(LongWord1, 4);
-  if LongWord1<>$46535000 then exit;
-  MemoryStream1.Position := 8;
+  InitPos := MemoryStream1.Position - 8;
   MemoryStream1.ReadBuffer(NameStart, 4);
+  NameStart := NameStart + InitPos;
   MemoryStream1.ReadBuffer(ValueStart, 4);
+  ValueStart := ValueStart + InitPos;
   MemoryStream1.ReadBuffer(NumOfEntries, 4);
 
-  SetLength(Result, 8);
   for LongWord1:=0 to NumOfEntries-1 do begin
-    MemoryStream1.Position := ($10 * LongWord1) + $14;
+    MemoryStream1.Position := ($10 * LongWord1) + $14 + InitPos;
     MemoryStream1.ReadBuffer(Word1, 2);
     MemoryStream1.Position := Word1 + NameStart;
-    MemoryStream1.ReadBuffer(Result[1], 8);
-    if Result='CATEGORY' then break;
+    MemoryStream1.ReadBuffer(Int641, 8);
+    if Int641=$59524F4745544143 then break;
   end;
-  MemoryStream1.Position := ($10 * LongWord1) + $20;
+  MemoryStream1.Position := ($10 * LongWord1) + $20 + InitPos;
   MemoryStream1.ReadBuffer(LongWord1, 4);
   MemoryStream1.Position := LongWord1 + ValueStart;
-  SetLength(Result, 2);
-  MemoryStream1.ReadBuffer(Result[1], 2);
+  MemoryStream1.ReadBuffer(Result, 2);
+end;
+
+function CheckKey(const ParamNum: LongWord): Boolean;
+var
+  MemoryStream1: TMemoryStream;
+  BytesStream1: TBytesStream;
+  ZDecompressionStream1: TZDecompressionStream;
+  LongWord1: LongWord;
+  i: Integer;
+  s: String;
+begin
+  Result := True;
+  s := Copy(ParamStr(ParamNum), 6);
+  case Length(s) of
+    0:;
+    32: begin
+      i:=1;
+      LongWord1:=0;
+      repeat
+        WorkbinKey[LongWord1] := HexToByte(Copy(s,i,2));
+        Inc(i, 2);
+        Inc(LongWord1);
+      until i>=Length(s);
+      WorkbinExist := 1;
+    end
+    else begin
+      MemoryStream1:=TMemoryStream.Create;
+      try
+        BytesStream1:=TBytesStream.Create(TNetEncoding.Base64.DecodeStringToBytes(s));
+        try
+          ZDecompressionStream1:=TZDecompressionStream.Create(BytesStream1, 10);
+          try
+            MemoryStream1.CopyFrom(ZDecompressionStream1, ZDecompressionStream1.Size);
+          finally ZDecompressionStream1.Free end;
+        finally BytesStream1.Free end;
+
+        if MemoryStream1.Size=512 then begin
+          MemoryStream1.Position := 0;
+          MemoryStream1.ReadBuffer(Workbin, 512);
+          WorkbinExist := 2;
+        end else begin
+          Writeln('Error: Invalid zRIF key');
+          Result := False;
+        end;
+      finally MemoryStream1.Free end;
+    end
+  end;
 end;
 
 const
@@ -74,97 +125,122 @@ const
   PkgVita3: array[0..15] of Byte = ($42,$3A,$CA,$3A,$2B,$D5,$64,$9F,$96,$86,$AB,$AD,$6F,$D8,$80,$1F);
   PkgVita4: array[0..15] of Byte = ($AF,$07,$FD,$59,$65,$25,$27,$BA,$F1,$33,$89,$66,$8B,$17,$D9,$EA);
   RifHdr: array[0..15] of Byte = (0,1,0,1,0,1,0,2,239,205,171,137,103,69,35,1);
-  ZeroByte: Byte=0;
+  PsfMagic: Int64 = $10146535000;
+  ZeroByte: Byte = 0;
 var
   Cipher: TDCP_rijndael;
   FileStream1, FileStream2: TFileStream;
-  MemoryStream1: TMemoryStream;
-  PkgKey, CtrKey, WorkbinKey: array [0..15] of Byte;
+  MemoryStream1, MemoryStream2: TMemoryStream;
+  PkgKey, CtrKey: array [0..15] of Byte;
   ItemCnt, NumOfFiles, NameOffset, NameSize, ListPos, HeadSize, SkuFlags, LongWord1: LongWord;
   DataOffset, ItemOffset, ItemSize, SomeFlags, Int641: Int64;
   KeyType: Byte;
-  LicenseFlags: Word;
+  LicenseFlags, Category: Word;
   utf8s: UTF8String;
-  WorkbinExist: Boolean;
   OutFolder, s, s2, s3: String;
   i: Integer;
+  BytesStream1: TBytesStream;
+  ZDecompressionStream1: TZDecompressionStream;
 begin
   try
-    Writeln('PS Vita PKG Unpacker v1.3 by RikuKH3');
+    Writeln('PS Vita PKG Unpacker v1.4 by RikuKH3');
     Writeln('------------------------------------');
-    if ParamCount=0 then begin Writeln('Usage: '+ExtractFileName(ParamStr(0))+' <input pkg file> [output folder] [-key=FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF]'); Readln; exit end;
-
-    WorkbinExist := False;
-    if ParamCount>1 then begin
-      if Pos('-key=', LowerCase(ParamStr(2)))=1 then begin
-        WorkbinExist := True;
-        s := Copy(ParamStr(2), 6);
-        if Length(s) mod 2 > 0 then s:='0'+s;
-        if Length(s)>0 then begin
-          i:=1;
-          LongWord1:=0;
-          repeat
-            WorkbinKey[LongWord1] := HexToByte(Copy(s,i,2));
-            Inc(i, 2);
-            Inc(LongWord1);
-          until i>=Length(s);
+    WorkbinExist := 0;
+    case ParamCount of
+      0: begin Writeln('Usage: '+ExtractFileName(ParamStr(0))+' <input pkg file> [output folder] [-key=FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF]'); Readln; exit end;
+      1: OutFolder:=ExpandFileName(Copy(ParamStr(1),1,Length(ParamStr(1))-Length(ExtractFileExt(ParamStr(1)))));
+      2: begin
+        if Pos('-key=', LowerCase(ParamStr(2)))=1 then begin
+          if CheckKey(2)=False then begin Readln; exit end;
+          OutFolder:=ExpandFileName(Copy(ParamStr(1),1,Length(ParamStr(1))-Length(ExtractFileExt(ParamStr(1)))));
+        end else begin
+          OutFolder := ExpandFileName(ParamStr(2));
+          repeat if OutFolder[Length(OutFolder)]='\' then SetLength(OutFolder, Length(OutFolder)-1) until not (OutFolder[Length(OutFolder)]='\');
         end;
-        OutFolder:=ExpandFileName(Copy(ParamStr(1),1,Length(ParamStr(1))-Length(ExtractFileExt(ParamStr(1)))));
-      end else begin
+      end
+      else begin
+        if Pos('-key=', LowerCase(ParamStr(3)))=1 then if CheckKey(3)=False then begin Readln; exit end;
         OutFolder := ExpandFileName(ParamStr(2));
         repeat if OutFolder[Length(OutFolder)]='\' then SetLength(OutFolder, Length(OutFolder)-1) until not (OutFolder[Length(OutFolder)]='\');
       end;
-    end else OutFolder:=ExpandFileName(Copy(ParamStr(1),1,Length(ParamStr(1))-Length(ExtractFileExt(ParamStr(1)))));
+    end;
 
     FileStream1:=TFileStream.Create(ParamStr(1), fmOpenRead or fmShareDenyWrite);
     try
-      FileStream1.Position := $14;
-      FileStream1.ReadBuffer(ItemCnt, 4);
-      ItemCnt := Swap32(ItemCnt);
-
       FileStream1.Position := $20;
       FileStream1.ReadBuffer(DataOffset, 8);
       DataOffset := Swap64(DataOffset);
 
-      FileStream1.Position := $37;
-      SetLength(utf8s, 9);
-      FileStream1.ReadBuffer(utf8s[1], 9);
-      OutFolder := OutFolder+'\'+string(utf8s)+'\';
-
-      FileStream1.Position := $70;
-      FileStream1.ReadBuffer(PkgKey[0], $10);
-
-      FileStream1.Position := $E7;
-      FileStream1.ReadBuffer(KeyType, 1);
-      KeyType := KeyType and 7;
-
-      case KeyType of
-        2: begin
-          Cipher:=TDCP_rijndael.Create(nil);
-          try
-            Cipher.Init(PkgVita2, 128, nil);
-            Cipher.EncryptECB(PkgKey, CtrKey);
-          finally Cipher.Free end
-        end;
-        3: begin
-          Cipher:=TDCP_rijndael.Create(nil);
-          try
-            Cipher.Init(PkgVita3, 128, nil);
-            Cipher.EncryptECB(PkgKey, CtrKey);
-          finally Cipher.Free end
-        end;
-        4: begin
-          Cipher:=TDCP_rijndael.Create(nil);
-          try
-            Cipher.Init(PkgVita4, 128, nil);
-            Cipher.EncryptECB(PkgKey, CtrKey);
-          finally Cipher.Free end
-        end
-      end;
-
-      FileStream1.Position := DataOffset;
+      FileStream1.Position := 0;
       MemoryStream1:=TMemoryStream.Create;
       try
+        MemoryStream1.CopyFrom(FileStream1, DataOffset);
+        MemoryStream1.Position := MemoryStream1.Position - 8;
+        repeat
+          MemoryStream1.ReadBuffer(Int641, 8);
+          if Int641=PsfMagic then break else MemoryStream1.Position:=MemoryStream1.Position-9;
+        until MemoryStream1.Position = 0;
+
+        Category := 0;
+        if Int641=PsfMagic then begin
+          Category:=ProcessSFO(MemoryStream1);
+        end;
+
+        MemoryStream1.Position := $37;
+        SetLength(utf8s, 9);
+        MemoryStream1.ReadBuffer(utf8s[1], 9);
+
+        case Category of
+          $6467: OutFolder := OutFolder+'\app\'+string(utf8s)+'\';   // gd
+          $7067: OutFolder := OutFolder+'\patch\'+string(utf8s)+'\'; // gp
+          $6361: begin                                               // ac
+            OutFolder := OutFolder+'\addcont\'+string(utf8s)+'\';
+            MemoryStream1.Position := $44;
+            SetLength(utf8s, $10);
+            MemoryStream1.ReadBuffer(utf8s[1], $10);
+            OutFolder := OutFolder+string(utf8s)+'\';
+          end
+          else OutFolder := OutFolder+'\'+string(utf8s)+'\'
+        end;
+
+        MemoryStream1.Position := $14;
+        MemoryStream1.ReadBuffer(ItemCnt, 4);
+        ItemCnt := Swap32(ItemCnt);
+
+        MemoryStream1.Position := $70;
+        MemoryStream1.ReadBuffer(PkgKey[0], $10);
+
+        MemoryStream1.Position := $E7;
+        MemoryStream1.ReadBuffer(KeyType, 1);
+        KeyType := KeyType and 7;
+
+        MemoryStream1.Clear;
+
+        case KeyType of
+          2: begin
+            Cipher:=TDCP_rijndael.Create(nil);
+            try
+              Cipher.Init(PkgVita2, 128, nil);
+              Cipher.EncryptECB(PkgKey, CtrKey);
+            finally Cipher.Free end
+          end;
+          3: begin
+            Cipher:=TDCP_rijndael.Create(nil);
+            try
+              Cipher.Init(PkgVita3, 128, nil);
+              Cipher.EncryptECB(PkgKey, CtrKey);
+            finally Cipher.Free end
+          end;
+          4: begin
+            Cipher:=TDCP_rijndael.Create(nil);
+            try
+              Cipher.Init(PkgVita4, 128, nil);
+              Cipher.EncryptECB(PkgKey, CtrKey);
+            finally Cipher.Free end
+          end
+        end;
+
+        FileStream1.Position := DataOffset;
         Cipher:=TDCP_rijndael.Create(nil);
         try
           if KeyType=1 then Cipher.Init(PkgKeyPSP, 128, nil) else Cipher.Init(CtrKey, 128, nil);
@@ -220,14 +296,8 @@ begin
         HeadSize := MemoryStream1.Size + DataOffset;
       finally MemoryStream1.Free end;
 
-      if FileExists(OutFolder+'sce_sys\param.sfo') then begin
-        MemoryStream1:=TMemoryStream.Create;
-        try
-          MemoryStream1.LoadFromFile(OutFolder+'sce_sys\param.sfo');
-          utf8s := ProcessSFO(MemoryStream1);
-        finally MemoryStream1.Free end;
-
-        if (utf8s='gd') or (utf8s='gp') then begin  // gd=app, ac=addcont, gp=patch
+      case Category of
+        $6467,$7067: begin // app, patch
           if DirectoryExists(OutFolder+'sce_sys\package\') then begin
             FileStream1.Position := FileStream1.Size - $1E0;
             FileStream2:=TFileStream.Create(OutFolder+'sce_sys\package\tail.bin', fmCreate or fmOpenWrite or fmShareDenyWrite);
@@ -242,58 +312,53 @@ begin
             finally FileStream2.Free end;
           end;
 
-          if WorkbinExist=False then if ParamCount>2 then if Pos('-key=', LowerCase(ParamStr(3)))=1 then begin
-            WorkbinExist := True;
-            s := Copy(ParamStr(3), 6);
-            if Length(s) mod 2 > 0 then s:='0'+s;
-            if Length(s)>0 then begin
-              i:=1;
-              LongWord1:=0;
-              repeat
-                WorkbinKey[LongWord1] := HexToByte(Copy(s,i,2));
-                Inc(i, 2);
-                Inc(LongWord1);
-              until i>=Length(s);
+          case WorkbinExist of
+            1: if Category=$6467 then begin // app
+              SkuFlags := 0;
+              LicenseFlags := $200;
+              SomeFlags := 0;
+              MemoryStream1:=TMemoryStream.Create;
+              try
+                if FileExists(OutFolder+'sce_sys\package\temp.bin') then begin
+                  MemoryStream1.LoadFromFile(OutFolder+'sce_sys\package\temp.bin');
+                  if MemoryStream1.Size=512 then begin
+                    MemoryStream1.Position := $FC;
+                    MemoryStream1.ReadBuffer(LongWord1, 4);
+                    if LongWord1=$1000000 then begin
+                      SkuFlags := $3000000;
+                      MemoryStream1.Position := 6;
+                      MemoryStream1.ReadBuffer(LicenseFlags, 2);
+                      MemoryStream1.Position := $98;
+                      MemoryStream1.ReadBuffer(SomeFlags, 8);
+                    end;
+                  end;
+                  MemoryStream1.Clear;
+                end;
+                MemoryStream1.WriteBuffer(RifHdr[0], 6);
+                MemoryStream1.WriteBuffer(LicenseFlags, 2);
+                MemoryStream1.WriteBuffer(RifHdr[8], 8);
+                FileStream1.Position := $30;
+                MemoryStream1.CopyFrom(FileStream1, $30);
+                for i:=0 to 15 do MemoryStream1.WriteBuffer(ZeroByte, 1);
+                MemoryStream1.WriteBuffer(WorkbinKey, $10);
+                for i:=0 to 55 do MemoryStream1.WriteBuffer(ZeroByte, 1);
+                MemoryStream1.WriteBuffer(SomeFlags, 8);
+                for i:=0 to 91 do MemoryStream1.WriteBuffer(ZeroByte, 1);
+                MemoryStream1.WriteBuffer(SkuFlags, 4);
+                for i:=0 to 255 do MemoryStream1.WriteBuffer(ZeroByte, 1);
+                MemoryStream1.SaveToFile(OutFolder+'sce_sys\package\work.bin');
+              finally MemoryStream1.Free end;
+            end;
+            2: if Category=$6467 then begin
+              MemoryStream1:=TMemoryStream.Create;
+              try
+                MemoryStream1.WriteBuffer(Workbin[0], 512);
+                MemoryStream1.SaveToFile(OutFolder+'sce_sys\package\work.bin');
+              finally MemoryStream1.Free end;
             end;
           end;
-
-          if WorkbinExist then if utf8s='gd' then begin
-            SkuFlags := 0;
-            LicenseFlags := $200;
-            SomeFlags := 0;
-            MemoryStream1:=TMemoryStream.Create;
-            try
-              if FileExists(OutFolder+'sce_sys\package\temp.bin') then begin
-                MemoryStream1.LoadFromFile(OutFolder+'sce_sys\package\temp.bin');
-                MemoryStream1.Position := $FC;
-                MemoryStream1.ReadBuffer(LongWord1, 4);
-                if LongWord1=$1000000 then begin
-                  SkuFlags := $3000000;
-                  MemoryStream1.Position := 6;
-                  MemoryStream1.ReadBuffer(LicenseFlags, 2);
-                  MemoryStream1.Position := $98;
-                  MemoryStream1.ReadBuffer(SomeFlags, 8);
-                end;
-                MemoryStream1.Clear;
-              end;
-              MemoryStream1.WriteBuffer(RifHdr[0], 6);
-              MemoryStream1.WriteBuffer(LicenseFlags, 2);
-              MemoryStream1.WriteBuffer(RifHdr[8], 8);
-              FileStream1.Position := $30;
-              MemoryStream1.CopyFrom(FileStream1, $30);
-              for i:=0 to 15 do MemoryStream1.WriteBuffer(ZeroByte, 1);
-              MemoryStream1.WriteBuffer(WorkbinKey, $10);
-              for i:=0 to 55 do MemoryStream1.WriteBuffer(ZeroByte, 1);
-              MemoryStream1.WriteBuffer(SomeFlags, 8);
-              for i:=0 to 91 do MemoryStream1.WriteBuffer(ZeroByte, 1);
-              MemoryStream1.WriteBuffer(SkuFlags, 4);
-              for i:=0 to 255 do MemoryStream1.WriteBuffer(ZeroByte, 1);
-              MemoryStream1.SaveToFile(OutFolder+'sce_sys\package\work.bin');
-            finally MemoryStream1.Free end;
-          end;
-        end else if utf8s='ac' then begin
-          if DirectoryExists(OutFolder+'sce_sys\package\') then TDirectory.Delete(OutFolder+'sce_sys\package\', True);
         end;
+        $6361: if DirectoryExists(OutFolder+'sce_sys\package\') then TDirectory.Delete(OutFolder+'sce_sys\package\', True);
       end;
     finally FileStream1.Free end;
     DeleteFile(OutFolder+'sce_pfs\pflist');
